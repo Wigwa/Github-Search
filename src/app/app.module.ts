3d8e76008830d146b640a8579de6328d40658c20
Wigwa <lucywigwa7@gmail.com>
2020-06-20 14:56:40 +0300

    add directiv file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitSearchComponent } from './git-search/git-search.component';
 import { CustomPipePipe } from './custom-pipe.pipe';
+import { CustomDirective } from './custom.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     GitSearchComponent,
-    CustomPipePipe
+    CustomPipePipe,
+    CustomDirective
   ],
   imports: [
     BrowserModule,
