c637b48cd6ece5d6851f9e131416c4720b4d5cda
Wigwa <lucywigwa7@gmail.com>
2020-06-22 10:39:06 +0300

    generate router

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey:'d42ea2f07f161c27a2668536453377506865c72b'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:'d42ea2f07f161c27a2668536453377506865c72b'
 };
 
 /*
